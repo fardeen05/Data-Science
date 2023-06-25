6f4e54c892dc5786592b7700392e3621a0ef12c6
fardeen05 <137701123+fardeen05@users.noreply.github.com>
2023-06-25 19:03:31 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("hello world!")
+
+
+# In[4]:
+
+
+print("Fardeen Khan")
+
+
+# In[5]:
+
+
+print("data science")
+
+
+# In[ ]:
+
+
+
+
